ef8a2189adca8f4da8642159875a4337dcb28660
ClaudiaInBytes <claudiainbytes@gmail.com>
2018-03-21 23:42:28 -0500

    First commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDHmFS9dwufDkOPEhxYMYZqGsisgN5LGtg",
-    authDomain: "the-breakfast-club-98607.firebaseapp.com",
-    databaseURL: "https://the-breakfast-club-98607.firebaseio.com",
-    projectId: "the-breakfast-club-98607",
-    storageBucket: "the-breakfast-club-98607.appspot.com",
-    messagingSenderId: "1061196400187"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
